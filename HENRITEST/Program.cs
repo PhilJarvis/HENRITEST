c0bebf91145a800e215329a84a530f39ba601595
PhilJarvis <Phil_Jarvis@hotmail.com>
2022-06-23 13:13:37 +0100

    Update Program.cs

--- HENRITEST/Program.cs
+++ HENRITEST/Program.cs
@@ -1,27 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-// Go to the Wesite
+// Go to the Website
 // *********************************************************************************
-IWebDriver driver = new ChromeDriver(@"C:\ChromeDriver\");
+IWebDriver driver = new ChromeDriver(@"C:\BrowserDrivers\Chrome\");
+driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
 driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
-string text = driver.Title;
-Console.WriteLine(text);
-System.Threading.Thread.Sleep(4000);
-// *********************************************************************************
+driver.Manage().Window.Maximize();
 
 // Select Dresses Menu
 driver.FindElement(By.CssSelector("#block_top_menu > ul > li:nth-child(2) > a")).Click();
 Thread.Sleep(10000);
 
 // Select most expensive dress
-driver.FindElement(By.CssSelector("#center_column > ul > li:nth-child(2) > div > div.left-block > div > a.product_img_link")).Click();
-Thread.Sleep(30000);
+driver.FindElement(By.CssSelector("#center_column > ul > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img")).Click();
 
 // Add to Cart
-driver.FindElement(By.CssSelector("#add_to_cart > button")).Click();
-System.Threading.Thread.Sleep(4000);
+var iframe = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div/iframe"));
+driver.SwitchTo().Frame(iframe);
+driver.FindElement(By.Name("Submit")).Click();
+
+// See if its added to Cart before closing
+Thread.Sleep(10000);
+
+// Close out
 driver.Close();
 
 
